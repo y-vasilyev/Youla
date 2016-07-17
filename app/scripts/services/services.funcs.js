bfaa60f3622eac98da9ec529b8197d3608747751
Quercitron <eugene.ivanov@gmail.com>
2016-07-17 23:06:17 +0300

    YOULACWD-13

--- app/scripts/services/services.funcs.js
+++ app/scripts/services/services.funcs.js
@@ -510,6 +510,7 @@
         	    var id = $stateParams.id;
         	    var entityType = 4;
 
+
         	    $scope.workHours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
         	    $scope.workMinutes = [0,10,20,30,40,50];
 
